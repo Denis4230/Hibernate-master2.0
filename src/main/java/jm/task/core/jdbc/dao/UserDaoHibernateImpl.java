9d896b39bc5656986022bd71b44b051427da1e79
Denis4230 <denis.semenov4230@gmail.com>
2022-07-07 11:59:25 +0700

    3

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -3,12 +3,14 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 import javax.persistence.PersistenceException;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+    private final SessionFactory connection = Util.getSessionFactory();
 
     public UserDaoHibernateImpl() {
 
@@ -17,74 +19,65 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-        Session session = Util.getSessionFactory().getCurrentSession();
-        try {
+        Session session = connection.openSession();
+        try (session) {
             session.beginTransaction();
             session.createSQLQuery("CREATE TABLE User (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(45) NULL, lastName VARCHAR(45) NULL, age INT NULL, PRIMARY KEY (id));")
                     .executeUpdate();
             session.getTransaction().commit();
-        } catch (PersistenceException | IllegalArgumentException ignore){
+        } catch (PersistenceException | IllegalArgumentException ignore) {
         } catch (Exception e) {
             e.getStackTrace();
             session.getTransaction().rollback();
-        } finally {
-            session.close();
         }
     }
 
     @Override
     public void dropUsersTable() {
-        Session session = Util.getSessionFactory().getCurrentSession();
-        try {
+        Session session = connection.openSession();
+        try (session) {
             session.beginTransaction();
             session.createSQLQuery("DROP TABLE IF EXISTS user").executeUpdate();
             session.getTransaction().commit();
 
-        } catch (IllegalArgumentException | PersistenceException ignore){
+        } catch (IllegalArgumentException | PersistenceException ignore) {
         } catch (Exception e) {
             e.getStackTrace();
             session.getTransaction().rollback();
-        } finally {
-            session.close();
         }
 
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        Session session = Util.getSessionFactory().openSession();
-        try {
+        Session session = connection.openSession();
+        try (session) {
             session.beginTransaction();
             User user = new User(name, lastName, age);
             session.save(user);
             session.getTransaction().commit();
             System.out.println("User - " + name + " добавлен");
 
-        } catch (IllegalArgumentException ignore){
         } catch (Exception e) {
             e.getStackTrace();
             session.getTransaction().rollback();
-        } finally {
-            session.close();
         }
 
     }
 
     @Override
     public void removeUserById(long id) {
-        Session session = Util.getSessionFactory().openSession();
-        try {
+        Session session = connection.openSession();
+        try (session) {
             session.beginTransaction();
             User user = session.get(User.class, id);
             session.delete(user);
             session.getTransaction().commit();
 
-        } catch (IllegalArgumentException ignore){
+        } catch (IllegalArgumentException ignore) {
         } catch (Exception e) {
             e.getStackTrace();
             session.getTransaction().rollback();
-        } finally {
-            session.close();
         }
 
 
@@ -93,35 +86,30 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public List<User> getAllUsers() {
         List<User> list = null;
-        Session session = Util.getSessionFactory().openSession();
-        try {
+        Session session = connection.openSession();
+        try (session) {
             Transaction transaction = session.beginTransaction();
             list = session.createCriteria(User.class).list();
             transaction.commit();
         } catch (Exception e) {
             e.getStackTrace();
             session.getTransaction().rollback();
-        } finally {
-            session.close();
         }
         return list;
     }
 
     @Override
     public void cleanUsersTable() {
-        Session session = Util.getSessionFactory().getCurrentSession();
-        try {
-
+        Session session = connection.openSession();
+        try (session) {
             session.beginTransaction();
             session.createQuery("DELETE FROM User").executeUpdate();
             session.getTransaction().commit();
 
-        } catch (IllegalArgumentException ignore){
+        } catch (IllegalArgumentException ignore) {
         } catch (Exception e) {
             e.getStackTrace();
             session.getTransaction().rollback();
-        } finally {
-            session.close();
         }
 
     }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -4,25 +4,15 @@ import jm.task.core.jdbc.model.User;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import java.sql.Connection;
-
 public class Util {
-    private static Connection connection;
-    private static final SessionFactory sessionFactory;
-
-    static  {
-        sessionFactory = new Configuration().addAnnotatedClass(User.class).buildSessionFactory();
-    }
-
-    public static Connection getConnection() {
-        return connection;
-    }
+    private static SessionFactory sessionFactory = null;
 
     public static SessionFactory getSessionFactory() {
-        return sessionFactory;
+        return sessionFactory = new Configuration().addAnnotatedClass(User.class).buildSessionFactory();
     }
 
     public static void closeSessionFacto() {
+        if (sessionFactory != null)
         sessionFactory.close();
     }
 }
